99ce8e17e3822c8e3d2a890010dbdcb92f0e3929
Benjamin <benjamintp.1985@gmail.com>
2025-11-26 17:55:53 +1100

    Create 3.2P & 4.2P folder structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# Task 1.3P
- A small Javascript web app that demonstrates the use of the onclick event handler to change the text of an h1 element when a button is clicked.
---
+++ seed.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/myprojectDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const ProjectSchema = new mongoose.Schema({
+
+});
+
+const Project = mongoose.model('Project', ProjectSchema);
+
+const sampleData = [
+
+];
+
+Project.insertMany(sampleData)
+  .then(() => {
+    console.log("Sample data inserted");
+    mongoose.connection.close();
+  })
+  .catch(err => console.error(err));
