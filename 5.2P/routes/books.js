688bc92d850b37ff31441b69a8edbdf4cddf8dd9
Benjamin <benjamintp.1985@gmail.com>
2025-12-08 20:07:04 +1100

    add book code for mvc pattern

---
+++ 5.2P/routes/books.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// import all controllers via index.js
+const Controllers = require('../controllers');
+
+// GET /books - Retrieve a list of all books
+router.get('/', Controllers.bookController.getAllBooks);
+
+module.exports = router;
---
+++ 5.2P/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// import route file
+const booksRoute = require('./routes/books');
+
+// mount the books route
+app.use('/books', booksRoute);
+
+// root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Book API');
+});
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
